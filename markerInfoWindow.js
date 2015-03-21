7d2ed9b5bc931642eabc01831c9f6030c58fcd83
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-21 14:03:47 +1100

    marker with info example added

---
+++ markerInfoWindow.js
@@ -0,0 +1,45 @@
+var initializeMap = function() {
+  var myLatlng = new google.maps.LatLng(41.89021, 12.492231);
+  var $container = $('#map-canvas')[0];
+  var mapOptions = {
+    zoom: 18,
+    // Same as satellite view but enables labels by default.
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    center: myLatlng
+  };
+  var map = new google.maps.Map($container, mapOptions);
+
+  // Sets the tilt of the imagery, doesn't work for standard map view.
+  map.setTilt(45);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      // Step 1 - Give it a relevant title.
+      title: 'Colosseum, Rome'
+  });
+
+  // Step 2 - Write your content.
+  var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Colosseum, Rome</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>The Colosseum</b> lives here.</p>'
+    '</div>'+
+    '</div>';
+
+  // Step 3 - Instantiate the info window.
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  // Step 4 - Add an event listener to the marker. The Google way.
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+}
+
+$(document).ready(function() {
+  initializeMap();
+});
