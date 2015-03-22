e1f8b2e5e155a6ef5ea9ca0363f6a1cf5f898a1d
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-23 10:57:07 +1100

    added rainbow for Joey

--- js/mapMarker.js
+++ js/mapMarker.js
@@ -1,17 +1,17 @@
 var initializeMap = function() {
   // Step 1 - Save the location of the marker.
-  var myLatlng = new google.maps.LatLng(51.500729, -0.124625);
+  var myLatlng = new google.maps.LatLng(-33.5964898, 151.32411739);
 
   var $container = $('#map-canvas')[0];
   var mapOptions = {
-    zoom: 18,
+    zoom: 15,
     center: myLatlng
   };
   var map = new google.maps.Map($container, mapOptions);
 
   // Optional - specify an image path relative to the project folder.
   // This can be used in place of the standard marker.
-  var image = 'img/bigben_256.png'
+  var image = 'img/rainbow.png'
 
   // Step 2 - Instantiate a new marker and tell it where to go.
   var marker = new google.maps.Marker({
